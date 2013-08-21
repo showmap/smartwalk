f29a046ea1973308e01e033d4ad9abcf811a2436
ievgen <ievgen.tiutiunnyk@gmail.com>
2013-08-20 20:57:20 -0700

    The cache folder is created if it doesn't exist

--- SmartWalkSolution/SmartWalk.Core/Services/CacheService.cs
+++ SmartWalkSolution/SmartWalk.Core/Services/CacheService.cs
@@ -19,13 +19,16 @@ namespace SmartWalk.Core.Services
 
         public string GetString(string key)
         {
-            var files = Directory.EnumerateFiles(_cacheFolderPath).ToArray();
-            var fileName = key + CacheFileExt;
-            var file = Path.Combine(_cacheFolderPath, fileName);
-
-            if (files.Contains(file))
+            if (Directory.Exists(_cacheFolderPath))
             {
-                return File.ReadAllText(file);
+                var files = Directory.EnumerateFiles(_cacheFolderPath).ToArray();
+                var fileName = key + CacheFileExt;
+                var file = Path.Combine(_cacheFolderPath, fileName);
+
+                if (files.Contains(file))
+                {
+                    return File.ReadAllText(file);
+                }
             }
 
             return null;
@@ -33,6 +36,11 @@ namespace SmartWalk.Core.Services
 
         public void SetString(string key, string value)
         {
+            if (!Directory.Exists(_cacheFolderPath))
+            {
+                Directory.CreateDirectory(_cacheFolderPath);
+            }
+
             var fileName = key + CacheFileExt;
 
             File.WriteAllText(Path.Combine(_cacheFolderPath, fileName), value);
