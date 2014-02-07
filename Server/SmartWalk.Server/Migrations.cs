38246a871c3b187752318629ad8340b38abbe777
sukretniy <sukretniy@gmail.com>
2014-02-06 19:52:47 -0800

    DataBase Nullables + Description lengths

--- Server/SmartWalk.Server/Migrations.cs
+++ Server/SmartWalk.Server/Migrations.cs
@@ -282,5 +282,122 @@ namespace SmartWalk.Server
             return 5;
         }
 
+        public int UpdateFrom5() {
+
+            SchemaBuilder.DropTable("EventMappingRecord");
+            SchemaBuilder.DropTable("EventMetaDataRecord");
+            SchemaBuilder.DropTable("EntityMappingRecord");
+            SchemaBuilder.DropTable("StorageRecord");
+            SchemaBuilder.DropTable("ShowRecord");
+            SchemaBuilder.DropTable("ContactRecord");
+            SchemaBuilder.DropTable("AddressRecord");
+            SchemaBuilder.DropTable("RegionRecord");
+            SchemaBuilder.DropTable("EntityRecord");
+
+            SchemaBuilder.CreateTable("EntityRecord", table => table
+              .Column("Id", DbType.Int32, column => column.PrimaryKey().Identity())
+              .Column("SmartWalkUserRecord_Id", DbType.Int32, c => c.NotNull())
+              .Column("Type", DbType.Byte, c => c.NotNull())
+              .Column("Name", DbType.String, c => c.NotNull())
+              .Column("Description", DbType.String, c => c.Nullable().Unlimited())
+              .Column("Picture", DbType.String, c => c.Nullable())
+              );
+
+            SchemaBuilder.CreateTable("RegionRecord", table => table
+              .Column("Id", DbType.Int32, column => column.PrimaryKey().Identity())
+              .Column("Country", DbType.String, c => c.NotNull())
+              .Column("State", DbType.String, c => c.NotNull())
+              .Column("City", DbType.String, c => c.NotNull())
+              );
+
+            SchemaBuilder.CreateTable("AddressRecord", table => table
+              .Column("Id", DbType.Int32, column => column.PrimaryKey().Identity())
+              .Column("EntityRecord_Id", DbType.Int32, c => c.NotNull())
+              .Column("RegionRecord_Id", DbType.Int32, c => c.NotNull())
+              .Column("Latitude", DbType.Double, c => c.NotNull())
+              .Column("Longitude", DbType.Double, c => c.NotNull())
+              .Column("Address", DbType.String, c => c.Nullable())
+              );
+
+            SchemaBuilder.CreateTable("ContactRecord", table => table
+              .Column("Id", DbType.Int32, column => column.PrimaryKey().Identity())
+              .Column("EntityRecord_Id", DbType.Int32, c => c.NotNull())
+              .Column("Type", DbType.Byte, c => c.NotNull())
+              .Column("Title", DbType.String, c => c.Nullable())
+              .Column("Contact", DbType.String, c => c.NotNull())
+              );
+
+            SchemaBuilder.CreateTable("ShowRecord", table => table
+              .Column("Id", DbType.Int32, column => column.PrimaryKey().Identity())
+              .Column("EntityRecord_Id", DbType.Int32, c => c.NotNull())
+              .Column("Title", DbType.String, c => c.Nullable())
+              .Column("Description", DbType.String, c => c.Nullable().Unlimited())
+              .Column("StartTime", DbType.DateTime, c => c.Nullable())
+              .Column("EndTime", DbType.DateTime, c => c.Nullable())
+              .Column("Picture", DbType.String, c => c.Nullable())
+              .Column("DetailsUrl", DbType.String, c => c.Nullable())
+              );
+
+            SchemaBuilder.CreateTable("StorageRecord", table => table
+              .Column("Id", DbType.Int32, column => column.PrimaryKey().Identity())
+              .Column("Key", DbType.String, c => c.NotNull())
+              .Column("Description", DbType.String, c => c.Nullable().Unlimited())
+              );
+
+            SchemaBuilder.CreateTable("EntityMappingRecord", table => table
+              .Column("Id", DbType.Int32, column => column.PrimaryKey().Identity())
+              .Column("EntityRecord_Id", DbType.Int32, c => c.NotNull())
+              .Column("StorageRecord_Id", DbType.Int32, c => c.NotNull())
+              .Column("ExternalEntityId", DbType.Int32, c => c.NotNull())
+              .Column("Type", DbType.Byte, c => c.NotNull())
+              );
+
+            SchemaBuilder.CreateTable("EventMetaDataRecord", table => table
+              .Column("Id", DbType.Int32, column => column.PrimaryKey().Identity())
+              .Column("RegionRecord_Id", DbType.Int32, c => c.NotNull())
+              .Column("EntityRecord_Id", DbType.Int32, c => c.NotNull())
+              .Column("SmartWalkUserRecord_Id", DbType.Int32, c => c.NotNull())
+              .Column("Title", DbType.String, c => c.Nullable())
+              .Column("Description", DbType.String, c => c.Nullable().Unlimited())
+              .Column("StartTime", DbType.DateTime, c => c.NotNull())
+              .Column("EndTime", DbType.DateTime, c => c.NotNull())
+              .Column("IsCombined", DbType.Boolean, c => c.NotNull())
+              .Column("IsMobileReady", DbType.Boolean, c => c.NotNull())
+              .Column("IsWidgetReady", DbType.Boolean, c => c.NotNull())
+              .Column("DateCreated", DbType.DateTime, c => c.NotNull())
+              .Column("DateModified", DbType.DateTime, c => c.NotNull())
+              );
+
+            SchemaBuilder.CreateTable("EventMappingRecord", table => table
+              .Column("Id", DbType.Int32, column => column.PrimaryKey().Identity())
+              .Column("EventMetaDataRecord_Id", DbType.Int32, c => c.NotNull())
+              .Column("StorageRecord_Id", DbType.Int32, c => c.NotNull())
+              .Column("ShowRecord_Id", DbType.Int32, c => c.NotNull())
+              );
+
+            SchemaBuilder.CreateForeignKey("EntityRecord_SmartWalkUserRecord", "EntityRecord", new[] { "SmartWalkUserRecord_Id" }, "SmartWalkUserRecord", new[] { "Id" });
+
+            SchemaBuilder.CreateForeignKey("AddressRecord_EntityRecord", "AddressRecord", new[] { "EntityRecord_Id" }, "EntityRecord", new[] { "Id" });
+            SchemaBuilder.CreateForeignKey("AddressRecord_RegionRecord", "AddressRecord", new[] { "RegionRecord_Id" }, "RegionRecord", new[] { "Id" });
+
+            SchemaBuilder.CreateForeignKey("ContactRecord_EntityRecord", "ContactRecord", new[] { "EntityRecord_Id" }, "EntityRecord", new[] { "Id" });
+
+            SchemaBuilder.CreateForeignKey("ShowRecord_EntityRecord", "ShowRecord", new[] { "EntityRecord_Id" }, "EntityRecord", new[] { "Id" });
+
+            SchemaBuilder.CreateForeignKey("EntityMappingRecord_EntityRecord", "EntityMappingRecord", new[] { "EntityRecord_Id" }, "EntityRecord", new[] { "Id" });
+            SchemaBuilder.CreateForeignKey("EntityMappingRecord_StorageRecord", "EntityMappingRecord", new[] { "StorageRecord_Id" }, "StorageRecord", new[] { "Id" });
+
+            SchemaBuilder.CreateForeignKey("EventMetaDataRecord_RegionRecord", "EventMetaDataRecord", new[] { "RegionRecord_Id" }, "RegionRecord", new[] { "Id" });
+            SchemaBuilder.CreateForeignKey("EventMetaDataRecord_EntityRecord", "EventMetaDataRecord", new[] { "EntityRecord_Id" }, "EntityRecord", new[] { "Id" });
+            SchemaBuilder.CreateForeignKey("EventMetaDataRecord_SmartWalkUserRecord", "EventMetaDataRecord", new[] { "SmartWalkUserRecord_Id" }, "SmartWalkUserRecord", new[] { "Id" });
+
+            SchemaBuilder.CreateForeignKey("EventMappingRecord_EventMetadataRecord", "EventMappingRecord", new[] { "EventMetadataRecord_Id" }, "EventMetadataRecord", new[] { "Id" });
+            SchemaBuilder.CreateForeignKey("EventMappingRecord_StorageRecord", "EventMappingRecord", new[] { "StorageRecord_Id" }, "StorageRecord", new[] { "Id" });
+            SchemaBuilder.CreateForeignKey("EventMappingRecord_ShowRecord", "EventMappingRecord", new[] { "ShowRecord_Id" }, "ShowRecord", new[] { "Id" });
+
+
+            return 6;
+        }
+
     }
 }
