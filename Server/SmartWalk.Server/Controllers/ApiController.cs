75171d894f2bac6357046d7de54190daee10897b
sukretniy <sukretniy@gmail.com>
2014-02-27 06:32:08 +0200

    SW-156 Json.NET serialization controller support

--- Server/SmartWalk.Server/Controllers/ApiController.cs
+++ Server/SmartWalk.Server/Controllers/ApiController.cs
@@ -1,13 +1,12 @@
 ﻿using System.Text;
 using System.Web.Mvc;
 using Newtonsoft.Json;
-using SmartWalk.Server.Controllers.Results;
 using SmartWalk.Server.Services.QueryService;
 using SmartWalk.Shared.DataContracts.Api;
 
 namespace SmartWalk.Server.Controllers
 {
-    public class ApiController : Controller
+    public class ApiController : BaseController
     {
         private readonly IQueryService _queryService;
 
@@ -20,25 +19,6 @@ namespace SmartWalk.Server.Controllers
         {
             var response = _queryService.ExecuteQuery(request);
             return Json(response);
-        }
-
-        // TODO: It doesn't work :-(
-        protected override JsonResult Json(
-            object data, 
-            string contentType, 
-            Encoding contentEncoding)
-        {
-            return new JsonNetResult
-                {
-                    Data = data,
-                    ContentType = contentType,
-                    ContentEncoding = contentEncoding,
-                    JsonSerializerSettings = new JsonSerializerSettings
-                        {
-                            Formatting = Formatting.Indented,
-                            NullValueHandling = NullValueHandling.Ignore
-                        }
-                };
         }
     }
 }
