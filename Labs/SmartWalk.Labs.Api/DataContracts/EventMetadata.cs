45c35a9509af1451e1211d5059c73e8e0ad2714a
ievgen <ievgen.tiutiunnyk@gmail.com>
2014-03-03 20:14:51 -0800

    SW-156 The example of deserialization of server data contracts was finished

---
+++ Labs/SmartWalk.Labs.Api/DataContracts/EventMetadata.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using SmartWalk.Shared.DataContracts;
+
+namespace SmartWalk.Labs.Api.DataContracts
+{
+    public class EventMetadata : IEventMetadata
+    {
+        public int Id { get; set; }
+        public Region Region { get; set; }
+        public Reference[] Host { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime? StartTime { get; set; }
+        public DateTime? EndTime { get; set; }
+        public CombineType? CombineType { get; set; }
+        public Reference[] Shows { get; set; }
+
+        IRegion IEventMetadata.Region
+        {
+            get { return Region; }
+            set { Region = (Region)value; }
+        }
+
+        IReference[] IEventMetadata.Host
+        {
+            get { return Host; }
+            set { Host = (Reference[])value; }
+        }
+
+        IReference[] IEventMetadata.Shows
+        {
+            get { return Shows; }
+            set { Shows = (Reference[])value; }
+        }
+    }
+}
---
+++ Labs/SmartWalk.Labs.Api/DataContracts/Reference.cs
@@ -0,0 +1,11 @@
+﻿using SmartWalk.Shared.DataContracts;
+
+namespace SmartWalk.Labs.Api.DataContracts
+{
+    public class Reference : IReference
+    {
+        public int Id { get; set; }
+        public string Storage { get; set; }
+        public int? Type { get; set; }
+    }
+}
