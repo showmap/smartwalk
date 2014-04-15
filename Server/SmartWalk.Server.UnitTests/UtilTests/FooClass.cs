8ba7b4cfee17891c2667a353527cdd72999dfdf2
ievgen <ievgen.tiutiunnyk@gmail.com>
2014-04-15 01:00:41 -0700

    SW-159 Unit tests added to make sure that Reflection refactoring is OK

---
+++ Server/SmartWalk.Server.UnitTests/UtilTests/FooClass.cs
@@ -0,0 +1,12 @@
+﻿namespace SmartWalk.Server.UnitTests.UtilTests
+{
+    public class FooClass
+    {
+        public string FooProperty { get; set; }
+
+        public string FooMethod(string foo)
+        {
+            return null;
+        }
+    }
+}
---
+++ Server/SmartWalk.Server.UnitTests/UtilTests/ReflectionTest.cs
@@ -0,0 +1,42 @@
+﻿using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SmartWalk.Shared.Utils;
+
+namespace SmartWalk.Server.UnitTests.UtilTests
+{
+    [TestClass]
+    public class ReflectionTest
+    {
+        [TestMethod]
+        public void TestHasProperty()
+        {
+            var foo = new FooClass();
+            Assert.IsTrue(foo.HasProperty("FooProperty"));
+        }
+
+        [TestMethod]
+        public void TestHasMethod()
+        {
+            var foo = new FooClass();
+            Assert.IsTrue(foo.HasMethod("FooMethod"));
+        }
+
+        [TestMethod]
+        public void TestGetValue()
+        {
+            var foo = new FooClass { FooProperty = "moo" };
+            Assert.AreEqual("moo", foo.GetValue<string>("FooProperty"));
+        }
+
+        [TestMethod]
+        public void TestGetElementType()
+        {
+            var foo1 = new FooClass { FooProperty = "moo" };
+            var foo2 = new FooClass { FooProperty = "moo" };
+            var foo3 = new FooClass { FooProperty = "moo" };
+            var coll = new[] { foo1, foo2, foo3 };
+            var elemType = coll.GetType().GetElementType();
+            Assert.AreEqual(typeof(FooClass), elemType);
+        }
+    }
+}
