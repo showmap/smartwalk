aff692e06a9433c5e8516504a559462480847a29
ievgen <ievgen.tiutiunnyk@gmail.com>
2014-04-22 19:52:08 -0700

    SW-167 The collapsing of long description texts was supported

--- Server/SmartWalk.Server/ResourceManifest.cs
+++ Server/SmartWalk.Server/ResourceManifest.cs
@@ -8,14 +8,16 @@ namespace SmartWalk.Server
     {
         public void BuildManifests(ResourceManifestBuilder builder)
         {
-            var manifest = builder.Add();      
-            
-            manifest.DefineScript("SmartWalk.AntiForgery").SetUrl("antiforgery.js");
-
-            manifest.DefineScript("ko.datetime").SetUrl("kodatetime.js").SetVersion("1.2").SetDependencies("ko", "SmartWalk.AntiForgery");
-            manifest.DefineScript("ko.autocomplete").SetUrl("autocomplete.js").SetVersion("1.2").SetDependencies("ko", "SmartWalk.AntiForgery");
+            var manifest = builder.Add();
 
             manifest.DefineScript("JQuery-1-10-2").SetUrl("jquery-1.10.2.js");
+
+            manifest.DefineScript("SmartWalk.Common").SetUrl("smartwalk-common.js").SetDependencies("jQuery");
+            manifest.DefineScript("SmartWalk.AntiForgery").SetUrl("antiforgery.js");
+
+            // TODO: Why these are depended on AntiForgery?
+            manifest.DefineScript("ko.datetime").SetUrl("kodatetime.js").SetVersion("1.2").SetDependencies("ko", "SmartWalk.AntiForgery");
+            manifest.DefineScript("ko.autocomplete").SetUrl("autocomplete.js").SetVersion("1.2").SetDependencies("ko", "SmartWalk.AntiForgery");
 
             manifest.DefineScript("SmartWalk.ViewModels").SetUrl("viewmodels.js").SetVersion("1.8").SetDependencies("ko.datetime", "ko.autocomplete");
 
---
+++ Server/SmartWalk.Server/Scripts/smartwalk-common.js
@@ -0,0 +1,24 @@
+﻿function collapsableText(config) {
+    var $descrText = $(config.targetSelector).first();
+    var relativeHeight = config.relativeSelector != null
+        ? $(config.relativeSelector).first().height()
+        : 0;
+    var collapsedHeight = Math.max(relativeHeight, 100);
+
+    if ($descrText.height() > collapsedHeight) {
+        $descrText.data("originalHeight", $descrText.height());
+        $descrText.addClass(config.gradientClass);
+        $descrText.css({ height: collapsedHeight + "px", cursor: "pointer" });
+
+        $descrText.click(function () {
+            if ($(this).height() > collapsedHeight) {
+                $(this).animate({ height: collapsedHeight }, 600, null, function () {
+                    $(this).addClass(config.gradientClass);
+                });
+            } else {
+                $(this).removeClass(config.gradientClass);
+                $(this).animate({ height: $(this).data("originalHeight") }, 600);
+            }
+        });
+    }
+}
