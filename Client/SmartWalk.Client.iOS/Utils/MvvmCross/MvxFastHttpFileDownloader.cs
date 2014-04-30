31f4f1bc533b8d932853ff8cf04e82f8aca8b460
ievgen <ievgen.tiutiunnyk@gmail.com>
2014-04-29 19:10:59 -0700

    SW-160 Restore a missing file
    How it got missing? Who knows, who knows…

---
+++ Client/SmartWalk.Client.iOS/Utils/MvvmCross/MvxFastHttpFileDownloader.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Cirrious.CrossCore.Core;
+using Cirrious.MvvmCross.Plugins.DownloadCache;
+
+namespace SmartWalk.Client.iOS.Utils.MvvmCross
+{
+    /// <summary>
+    /// As seen on http://www.michaelridland.com/mobile/implementing-modernhttpclient-in-mvvmcross/
+    /// </summary>
+    public class MvxFastHttpFileDownloader : MvxLockableObject, IMvxHttpFileDownloader
+    {
+        private const int DefaultMaxConcurrentDownloads = 30;
+
+        private readonly Dictionary<MvxFastFileDownloadRequest, bool> _currentRequests =
+            new Dictionary<MvxFastFileDownloadRequest, bool>();
+        private readonly Queue<MvxFastFileDownloadRequest> _queuedRequests = 
+            new Queue<MvxFastFileDownloadRequest>();
+
+        private readonly int _maxConcurrentDownloads;
+
+        public MvxFastHttpFileDownloader(
+            int maxConcurrentDownloads = DefaultMaxConcurrentDownloads)
+        {
+            _maxConcurrentDownloads = maxConcurrentDownloads;
+        }
+
+        public void RequestDownload(
+            string url, 
+            string downloadPath, 
+            Action success, 
+            Action<Exception> error)
+        {
+            var request = new MvxFastFileDownloadRequest(url, downloadPath);
+
+            request.DownloadComplete += 
+                (sender, args) =>
+                {
+                    OnRequestFinished(request);
+                    success();
+                };
+
+            request.DownloadFailed += 
+                (sender, args) =>
+                {
+                    OnRequestFinished(request);
+                    error(args.Value);
+                };
+
+            RunSyncOrAsyncWithLock(
+                () =>
+                {
+                    _queuedRequests.Enqueue(request);
+
+                    if (_currentRequests.Count < _maxConcurrentDownloads)
+                    {
+                        StartNextQueuedItem();
+                    }
+                });
+        }
+
+        private void OnRequestFinished(MvxFastFileDownloadRequest request)
+        {
+            RunSyncOrAsyncWithLock(
+                () =>
+                {
+                    _currentRequests.Remove(request);
+
+                    if (_queuedRequests.Any())
+                    {
+                        StartNextQueuedItem();
+                    }
+                });
+        }
+
+        private void StartNextQueuedItem()
+        {
+            if (_currentRequests.Count >= _maxConcurrentDownloads) return;
+
+            RunSyncOrAsyncWithLock(
+                () =>
+                {
+                    if (_currentRequests.Count >= _maxConcurrentDownloads) return;
+                    if (!_queuedRequests.Any()) return;
+
+                    var request = _queuedRequests.Dequeue();
+                    _currentRequests.Add(request, true);
+                    request.Start();
+                });
+        }
+    }
+}
