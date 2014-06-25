91969ae69406fd4008ce66dc7642181950578103
ievgen <ievgen.tiutiunnyk@gmail.com>
2014-06-24 19:26:54 -0700

    SW-159 Finishing rfctring of Hosts/View copy-paste

--- Server/SmartWalk.Server/ViewModels/ListViewParametersVm.cs
+++ Server/SmartWalk.Server/ViewModels/ListViewParametersVm.cs
@@ -1,25 +1,31 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using SmartWalk.Server.Extensions;
 
 namespace SmartWalk.Server.ViewModels
 {
     public class ListViewParametersVm
     {
-        public SortType Sort { get; set; }
         private DisplayType _display;
-        public DisplayType Display {
+
+        public SortType Sort { get; set; }
+
+        public DisplayType Display
+        {
             get { return IsLoggedIn ? _display : DisplayType.All; }
             set { _display = value; }
         }
+
         public bool IsLoggedIn { get; set; }
-        public string Url {
-            get {
-                switch (Display) {
+
+        public string Url
+        {
+            get
+            {
+                switch (Display)
+                {
                     case DisplayType.All:
                         return "";
+
                     case DisplayType.My:
                         return "/" + DisplayType.My.ToString().ToLower();
                 }
@@ -28,25 +34,31 @@ namespace SmartWalk.Server.ViewModels
             }
         }
 
-
-        public ListViewParametersVm() {
+        public ListViewParametersVm()
+        {
             Sort = SortType.Date;
             _display = DisplayType.My;
             IsLoggedIn = false;
         }
 
-        public void LoadParameters(bool isLoggedIn, string sort) {
+        public void LoadParameters(bool isLoggedIn, string sort)
+        {
             IsLoggedIn = isLoggedIn;
-            Sort = String.IsNullOrEmpty(sort) ? SortType.Date : (SortType)Enum.Parse(typeof (SortType), sort.ToUpperFirstLetter());
+            Sort =
+                String.IsNullOrEmpty(sort)
+                    ? SortType.Date
+                    : (SortType)Enum.Parse(typeof(SortType), sort.ToUpperFirstLetter());
         }
     }
 
-    public enum SortType {
+    public enum SortType
+    {
         Title,
         Date
     }
 
-    public enum DisplayType {
+    public enum DisplayType
+    {
         All,
         My
     }
